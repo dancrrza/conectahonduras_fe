e2ab279b45bc827b31ed0c146d2d0517c7a654b3
suhaibbaba <suhaib.m.baba@gmail.com>
2026-03-11 20:42:46 +0200

    Fix forgot password

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import { ForgotPasswordForm } from "@/components/auth/forgot-password-form";
 export default function Page() {
   return (
     <div className="flex-1 flex items-center justify-center px-5 py-16 relative">
-      <div className="relative z-10 w-full max-w-md">
+      <div className="relative z-10 w-full max-w-sm">
         <ForgotPasswordForm />
       </div>
     </div>
